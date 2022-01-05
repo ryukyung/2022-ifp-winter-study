37dfa7b7e36276593f8751116480637a47841eae
hyeon0697 <72971346+hyeon0697@users.noreply.github.com>
2022-01-05 16:16:16 +0900

    Create 코딩도장3-8심사문제.py

---
+++ CodingPy/Ch3/코딩도장3-8심사문제.py
@@ -0,0 +1,11 @@
+#'Hello, world!' 두 개를 각 줄에 출력하는 프로그램을 만드세요(대소문자 구분과 띄어쓰기가 정확해야 합니다). 정답에는 출력 결과를 만족하는 전체 소스 코드를 입력해야 합니다.
+# ex) Hello, world!
+#     Hello, world!
+# 문제 제출 코딩도장 Python
+# 답안 제출 2022/01/05 16:13 경준현
+
+print("Hello, world!")
+print("Hello, world!")
+
+#답안 풀이
+#print문으로 Hello, world! 한줄 출력하고, 다음 줄에 똑같은 print문으로 Hello, world!를 출력하였다.
