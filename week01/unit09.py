1baf8415fe81fd1d57e35fbf3ed0c336875bf523
taezero <rlaxodud911@gmail.com>
2022-01-07 02:48:59 +0900

    create week01 and add its contents

---
+++ week01/unit09.py
@@ -0,0 +1,22 @@
+# 9.4 심사 문제 : 여러 줄로 된 문자열 사용하기
+
+'''
+다음 소스 코드를 완성하여 실행 결과대로 문자열이 출력되게 만드세요.
+
+
+____________________
+____________________
+____________________
+print(s)
+# 결과 :
+# 'Python' is a "programming language"
+# that lets you work quickly
+# and
+# integrate systems more effectively.
+'''
+
+# 풀이
+s = """'Python' is a "programming language"
+that lets you work quickly
+and
+integrate systems more effectively."""
