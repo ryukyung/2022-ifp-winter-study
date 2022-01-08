71d2fe91d77b76f4e797e663ca177d95299d0ba4
jongmung <jmp99@naver.com>
2022-01-09 03:52:50 +0900

    Create Unit03-12핵심정리.py

---
+++ Unit03-12핵심정리.py
@@ -0,0 +1,19 @@
+#Unit03
+#Hello, world! 출력해보기
+print('Hello, world!')
+
+#연습문제:문자열 출력하기
+#다음 소스 코드를 완성하여 'Hello, world!'와 'Python Programming'이 각 줄에 출력되게 만드세요.
+#print('Hello, world!') -> Hello, world!
+#print(               ) -> Python Programming
+print('Python Programming')
+
+#문자열 출력하기
+#Hello, world!
+#Hello, world! 나오게 소스작성
+print('Hello, world!')
+print('Hello, world!')
+
+#Unit04
+k = 10 / 4
+print(k)
