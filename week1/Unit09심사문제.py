997a1ae979af29e3dedd089990a784c011e6a9cb
jongmung <jmp99@naver.com>
2022-01-09 21:48:05 +0900

    week1

---
+++ week1/Unit09심사문제.py
@@ -0,0 +1,10 @@
+#Unit09
+#결과대로 문자열이 출력되게 작성
+#'Python' is a "programming language"
+#that lets you work quickly
+#and
+#integrate systems more effectively.
+s = ''''Python' is a "programming language"
+that lets you work quickly
+and
+integrate systems more effectively.'''
