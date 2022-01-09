4d060c96fdd6ee619ddc1a91a6765a0941abca6d
CHAE HEEJIN <96931010+coowooc@users.noreply.github.com>
2022-01-10 01:01:10 +0900

    codingdojang_unit3-12

---
+++ 코딩도장-unit3.6.py
@@ -0,0 +1,8 @@
+#코딩도장-unit3.6
+#Hello, world 출력하기
+
+print("Hello, world")
+
+#실행결과
+#Hello, wolrd
+#따옴표 열고닫기 꼭 확인하기!
---
+++ 코딩도장-unit9.4.py
@@ -0,0 +1,9 @@
+#코딩도장-unit9.4
+#다음 소스 코드를 완성하여 실행 결과대로 문자열이 출력되게 만드세요.
+#실행결과 
+#'Python' is a "programming language" that lets you quickly and integrate systems more effectively.
+
+print(''''Python' is a "programming language" that lets you work quickly and integrate systems more effectively''')
+#'Python' is a "programming language" that lets you quickly and integrate systems more effectively.
+
+#여러 줄로 된 문자열은 작은따옴표 안에 작은따옴표와 큰따옴표를 둘 다 넣을 수 있음
