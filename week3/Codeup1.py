b26a48cad14327a12b1ef01282ab5da40e04966e
jongmung <jmp99@naver.com>
2022-01-19 23:29:50 +0900

    week3-1

---
+++ week3/Codeup1.py
@@ -0,0 +1,49 @@
+#1
+#Hello
+#출력하기
+print('Hello')
+
+#2
+#Hello World
+#출력하기
+print('Hello World')
+
+#3
+#Hello
+#world
+#두 줄에 걸쳐 줄을 바꿔 출력하기
+print('''\
+Hello
+World
+''')
+
+#4
+#'Hello'
+#작은 따옴표도 같이 출력
+print("'Hello'")
+
+#5
+#"Hello World"
+#큰 따옴표도 함께 출력
+print('"Hello World')
+
+#6
+#"!@#$%^&*()"
+#큰 따옴표도 함께 출력
+print('"!@#$%^&*()"')
+
+#7
+#"C:\Download\hello.cpp"
+#큰 따옴표도 함께 출력
+print('"C:\Download\hello.cpp"')
+
+#8
+#┌┬┐
+#├┼┤
+#└┴┘
+#출력하기
+print('''\
+┌┬┐
+├┼┤
+└┴┘
+''')
