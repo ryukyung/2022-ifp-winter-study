58813637ac0c1aa4bd9e7625b73778bd624c7409
ryukyung <ryukyung123@naver.com>
2022-01-22 21:16:24 +0900

    류경혜 코드업 기초 100제 1~6강 제출합니다

--- codeUp1-6/codeup03.py
+++
@@ -1,9 +0,0 @@
-# [28] 정수 1개를 입력받아 그대로 출력해보자
-inum = int(input())
-print(inum)
-# [29] 실수 1개를 입력받아 그대로 출력해보자
-fnum = float(input())
-print(fnum)
-# [30] 정수 1개를 입력받아 그대로 출력해보자
-num = int(input())
-print(num)
