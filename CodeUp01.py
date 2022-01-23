375ed6d1b706bd47b4117a9c23f157e831533ea5
N0Rib <youkd2562@gmail.com>
2022-01-23 14:42:47 +0900

    유경덕 코드업 기초 100제 1~6강 제출합니다

---
+++ CodeUp01.py
@@ -0,0 +1,44 @@
+# Hello
+# 출력하기
+
+print('Hello')
+
+# Hello World
+# 출력하기
+
+print('Hello World')
+
+# Hello
+# World
+# (두 줄에 걸쳐 줄을 바꿔 출력하기)
+
+print('''\
+Hello
+World
+''')
+
+# 'Hello'
+# (단, 작은 따옴표도 함께 출력한다.)
+
+print("'Hello'")
+
+# "!@#$%^&*()"
+# (단, 큰따옴표도 함께 출력한다.)
+
+print('"!@#$%^&*()"')
+
+# "C:\Download\hello.cpp"
+# (단, 큰따옴표도 함께 출력한다.)
+
+print('"C:\Download\hello.cpp"')
+
+# ┌┬┐
+# ├┼┤
+# └┴┘
+# 출력하기
+
+print('''\
+┌┬┐
+├┼┤
+└┴┘
+''')
