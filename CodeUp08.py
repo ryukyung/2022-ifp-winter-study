77b51f0774efddad019242e009475105fd821ce8
N0Rib <youkd2562@gmail.com>
2022-01-29 02:35:54 +0900

    유경덕 코드업 기초 100제 07~10강 제출합니다

---
+++ CodeUp08.py
@@ -0,0 +1,39 @@
+# 1(true, 참) 또는 0(false, 거짓) 이 입력되었을 때 반대로 출력하는 프로그램을 작성해보자.
+
+boolean = int(input())
+print( not boolean )
+
+# 두 개의 참(1) 또는 거짓(0)이 입력될 때, 모두 참일 때에만 참을 출력하는 프로그램을 작성해보자.
+
+for _ in range(4):
+  a, b = map(int, input().split())
+  print( a and b )
+
+# 두 개의 참(1) 또는 거짓(0)이 입력될 때, 하나라도 참이면 참을 출력하는 프로그램을 작성해보자.
+
+for i in range(4):
+  a, b = map(int, input().split())
+  print( a or b )
+
+# 1개의 정수형 입력이 들어오면 논리 연산을 이용하여 '홀수'와 '짝수'를 판별하여라
+
+number = int(input())
+print( number%2 and '홀수' or '짝수' )
+
+# 두 가지의 참(1) 또는 거짓(0)이 입력될 때, 참/거짓이 서로 다를 때에만 참을 출력하는 프로그램을 작성해보자.
+
+for _ in range(4):
+  a, b = map(int, input().split())
+  print( (a and (not b)) or ((not a) and b) )
+
+# 두 개의 참(1) 또는 거짓(0)이 입력될 때, 참/거짓이 서로 같을 때에만 참이 계산되는 프로그램을 작성해보자.
+
+for _ in range(4):
+  a, b = map(int, input().split())
+  print( ((not a) and (not b)) or (a and b) )
+
+# 두 개의 참(1) 또는 거짓(0)이 입력될 때, 모두 거짓일 때에만 참이 계산되는 프로그램을 작성해보자.
+
+for _ in range(4):
+  a, b = map(int, input().split())
+  print( not(a or b) )
---
+++ CodeUp10.py
@@ -0,0 +1,16 @@
+# 입력된 두 정수 a, b 중 큰 값을 출력하는 프로그램을 작성해보자.
+# 단, 조건문을 사용하지 않고 3항 연산자 'and or' 를 사용한다.
+
+a, b = map(int, input().split())
+print( a>b and a or b )
+
+# 입력된 세 정수 a, b, c 중 가장 작은 값을 출력하는 프로그램을 작성해보자. (단, 삼항 연산자 이용)
+
+a, b, c = map(int, input().split())
+num = a if a<b else b
+print(  num if num < c else c )
+
+# 1개의 정수형 입력이 들어오면 삼항 연산을 이용하여 '홀수'와 '짝수'를 판별하여라
+
+number = int(input())
+print( '홀수' if number%2 else '짝수' )
