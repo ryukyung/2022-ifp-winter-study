13fe9ad73a9ad191baa43b5debfc08ab5cec243c
blooper20 <yjw127588@gmail.com>
2022-02-03 18:11:30 +0900

    유재우 5주차 12강

--- 코드업 기초 100제/코드업문제70.py
+++ 코드업 기초 100제/코드업문제70.py
@@ -1,10 +1,11 @@
 # 정수가 순서대로 입력된다.
 # (단, 개수는 알 수 없다.)
 # 0이 아니면 입력된 정수를 출력하고, 0이 입력되면 출력을 중단해보자.
-# while( ), for( ) 등의 반복문을 사용할 수 없다.
+
 # 입력
 # 정수가 순서대로 입력된다.
 # 7 4 2 3 0 1 5 6 9 10 8
+
 # 출력
 # 입력된 정수를 줄을 바꿔 하나씩 출력하는데, 0이 입력되면 종료한다. (0은 출력하지 않는다.)
 # 7
@@ -13,14 +14,21 @@
 # 3
 # 내 풀이)
 num = map(int, input().split())
-
+for i in num:
+    if i == 0:
+        break
+    print(i)
 # 강의 풀이)
-def goto(array, i):
-  if array[i] == 0:
-    return
-  print(array[i])
-  i += 1
-  goto(array, i)
+# 1
+number = map(int, input().split())
+for element in number:
+  if element is not 0:
+    print(element)
+    continue # continue를 만나면 아래의 구문은 실행하지 않고 다음 반복으로 넘어간다.
+  break
 
-array = list(map(int, input().split()))
-goto(array, i = 0)
+# 2
+number = map(int, input().split())
+for value in number:
+  if value == 0: break
+  print( value )
