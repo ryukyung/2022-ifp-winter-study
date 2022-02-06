8c9fdc217cb4be4eaf62771a27bc25a306d6562a
chaeeunj <official2930@naver.com>
2022-02-06 17:57:03 +0900

    83수정

--- week05/파이썬코드업기초13-종합/83.py
+++ week05/파이썬코드업기초13-종합/83.py
@@ -7,8 +7,3 @@ num = int(input())
 for i in range(1, num+1):
   count = i if i%3 else 'X'
   print(count, end=' ')
-
-#2
-for i in range(1, num+1):
-  count = 'X' if '3' in str(i) or '6' in str(i) or '9' in str(i) else i
-  print(count, end=' ')
